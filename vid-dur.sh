a881ccd0e8fca90bdbb515fb1c41aee6ca60795d
Ahmad Nawaz <ahmad.nawaz@ahmad.nawaz-MBP15-8111>
2019-10-17 08:17:37 -0700

    first commit

---
+++ vid-dur.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sum=0
+while read line; do
+	duration=$(mdls -name kMDItemDurationSeconds "$line" | cut -d "=" -f 2)
+	sum=$(echo "$duration+$sum"|bc)
+done <<< "$(find .  -type f -name "*.mp4")"
+h=$(bc <<< "$sum/3600")
+m=$(bc <<< "($sum%3600)/60")
+s=$(bc <<< "$sum%60")
+printf "%02d:%02d:%05.2f\n" $h $m $s
